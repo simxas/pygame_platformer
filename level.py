1a632a661781faf61445c0ae78f7a1805cbcbd9a
simxas <darbolickus@gmail.com>
2022-10-01 10:08:46 +0300

    added gravity and jump, no collisions

--- level.py
+++ level.py
@@ -32,7 +32,7 @@ class Level:
         player_x = player.rect.centerx
         direction_x = player.direction.x
 
-        if player_x < screen_width / 4 and direction_x < 0:
+        if player_x < (screen_width / 4) and direction_x < 0:
             self.world_shift = 8
             player.speed = 0
         elif player_x > screen_width - (screen_width / 4) and direction_x > 0:
--- player.py
+++ player.py
@@ -6,8 +6,12 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32, 64))
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft=pos)
+        #region player movement
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = 8
+        self.gravity = 0.8
+        self.jump_speed = -16
+        #endregion
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -19,8 +23,19 @@ class Player(pygame.sprite.Sprite):
             self.direction.x = -1
         else:
             self.direction.x = 0
+
+        if keys[pygame.K_SPACE]:
+            self.jump()
         #endregion
+
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+
+    def jump(self):
+        self.direction.y = self.jump_speed
 
     def update(self):
         self.get_input()
         self.rect.x += self.direction.x * self.speed
+        self.apply_gravity()
