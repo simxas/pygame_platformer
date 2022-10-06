8754139c8e22dd090aa0f8ebe845affa40c6904e
simxas <darbolickus@gmail.com>
2022-10-06 12:24:12 +0300

    working animations

--- player.py
+++ player.py
@@ -5,8 +5,11 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         self.import_character_assets()
-        self.image = pygame.Surface((32, 64))
-        self.image.fill('red')
+        self.frame_index = 0
+        self.animation_speed = 0.15
+        self.image = self.animations['idle'][self.frame_index]
+        # self.image = pygame.Surface((32, 64))
+        # self.image.fill('red')
         self.rect = self.image.get_rect(topleft=pos)
         #region player movement
         self.direction = pygame.math.Vector2(0, 0)
@@ -22,6 +25,16 @@ class Player(pygame.sprite.Sprite):
         for animation in self.animations.keys():
             full_path = character_path + animation
             self.animations[animation] = import_folder(full_path)
+
+    def animate(self):
+        animation = self.animations['idle']
+
+        # loop over frame index
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+
+        self.image = animation[int(self.frame_index)]
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -47,3 +60,4 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.get_input()
+        self.animate()
